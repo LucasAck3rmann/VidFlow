12e0dbdf3fbc61ce42fdd5820faa611af53aaf66
Lucas Ackermann <ackermannlucas25@gmail.com>
2024-06-24 21:40:05 -0300

    Atualização Javascript

---
+++ script.js
@@ -0,0 +1,36 @@
+"videos": [
+    {
+      "id": 1,
+      "titulo": "Conhecendo a linguagem Go | Hipsters.Talks",
+      "descricao": "3 mil visualizações",
+      "url": "https://www.youtube.com/embed/y8FeZMv37WU",
+      "imagem": "https://github.com/MonicaHillman/aluraplay-requisicoes/blob/main/img/logo.png?raw=true",
+      "categoria": "Programação"
+    },
+    {
+
+        "id": 2,
+        "titulo": "Conhecendo a linguagem Go | Hipsters.Talks",
+        "descricao": "3 mil visualizações",
+        "url": "https://www.youtube.com/embed/y8FeZMv37WU",
+        "imagem": "
+        "categoria": "Programação"
+        },
+        {
+        "id": 3,
+        "titulo": "Conhecendo a linguagem Go | Hipsters.Talks",
+        "descricao": "3 mil visualizações",
+        "url": "https://www.youtube.com/embed/y8FeZMv37WU",
+        "imagem": "
+        "categoria": "Programação"
+        },
+        {
+        "id": 4,
+        "titulo": "Conhecendo a linguagem Go | Hipsters.Talks",
+        "descricao": "3 mil visualizações",
+        "url": "https://www.youtube.com/embed/y8FeZMv37WU",
+        "imagem": "
+        "categoria": "Programação"
+        },
+        {
+            
